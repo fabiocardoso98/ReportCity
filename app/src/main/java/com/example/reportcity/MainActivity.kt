7fa386a28704ca9ce5458cf4e4e489b7405c0cc6
Fábio Cardoso <faavmica@hotmail.com>
2021-03-24 14:30:51 +0000

    create notes

--- app/src/main/java/com/example/reportcity/MainActivity.kt
+++ app/src/main/java/com/example/reportcity/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.reportcity
 
-import android.support.v7.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 }
